4bd315806b219baaa6aa8bc337990003364ef4e3
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-09-29 13:13:56 +0200

    Feature: check throw error when mi si bigger than max

--- 03/app.js
+++ 03/app.js
@@ -1,7 +1,9 @@
 export default function randomNumber(min, max) {
- if (!isNaN(min) && !isNaN(max)) {
-  return 1;
- } 
- throw new Error("Range is not a number");
- 
+  if (min > max) {
+    throw new Error("Min value is bigger than max value");
+  } else if (!isNaN(min) && !isNaN(max)) {
+    return 1;
+  }
+
+  throw new Error("Range is not a number");
 }
