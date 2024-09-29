3344a415274ef264800153e7fa72d9dda3295954
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-09-29 18:28:54 +0200

    Feature: throw exception when email is incorrect

--- 04/User.js
+++ 04/User.js
@@ -1,8 +1,18 @@
 export default class User {
   constructor({ email, password }) {
-    this.email = email;
+    this.email = email
     this.password = password;
+    this.validateEmail(this.email)
   }
+
+
+ validateEmail (email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const isValid = emailRegex.test(email);
+  if (!isValid) {
+    throw new Error("E-mail niepoprawny");
+  }
+ }
   getEmail() {
     return this.email;
   }
