10aa277e8c4e4455a8c84c2227fe420398b388f8
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-09-29 13:44:33 +0200

    Frature: test random number form range min-max

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,7 @@
 export default function randomNumber(min, max) {
   if (min > max || isNaN(min) || isNaN(max)) {
     throw new Error("Range is incorrect");
+  } else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return 1;
 }
