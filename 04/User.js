e0cf2e674f6cb2dfd9c49d412dfeb9627d24f516
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-09-29 18:32:53 +0200

    Feature: throw exception when password is incorrect

--- 04/User.js
+++ 04/User.js
@@ -1,23 +1,30 @@
 export default class User {
   constructor({ email, password }) {
-    this.email = email
+    this.email = email;
     this.password = password;
-    this.validateEmail(this.email)
+    this.validateEmail(this.email);
+    this.validatePassword(this.password);
   }
 
-
- validateEmail (email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const isValid = emailRegex.test(email);
-  if (!isValid) {
-    throw new Error("E-mail niepoprawny");
-  }
- }
   getEmail() {
     return this.email;
   }
 
   getPassword() {
     return this.password;
+  }
+
+  validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(email);
+    if (!isValid) {
+      throw new Error("E-mail niepoprawny");
+    }
+  }
+
+  validatePassword(password) {
+    if (password !== 'pw123456') {
+      throw new Error("Hasło niepoprawne");
+    }
   }
 }
