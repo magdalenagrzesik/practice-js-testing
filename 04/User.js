38a70bb6d70f381ceebaec9298d0c1c9b136a518
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-09-29 17:54:11 +0200

    Feature: had set email and password when create instance

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,13 @@
 export default class User {
+  constructor({ email, password }) {
+    this.email = email;
+    this.password = password;
+  }
+  getEmail() {
+    return this.email;
+  }
 
+  getPassword() {
+    return this.password;
+  }
 }
