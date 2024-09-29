01afd5a785f8244f94bd0dd97b086c00a535121b
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-09-29 18:44:55 +0200

    Feature: return true when email contain domain devmentor.pl

--- 04/User.js
+++ 04/User.js
@@ -4,6 +4,7 @@ export default class User {
     this.password = password;
     this.validateEmail(this.email);
     this.validatePassword(this.password);
+    this.login()
   }
 
   getEmail() {
@@ -27,4 +28,20 @@ export default class User {
       throw new Error("Hasło niepoprawne");
     }
   }
+
+  validateDomain(email) {
+    const allowed = ['devmentor.pl']
+    const domain = email.split('@')[1]
+    const isValid = allowed.includes(domain)
+    if (isValid) {
+      return true
+    }
+  }
+
+  login() {
+    if(this.validateDomain(this.email)) {
+      return true
+    }
+  }
+
 }
