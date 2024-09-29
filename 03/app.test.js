67451c7163d7c3d81b197346c6a47e8913e23cba
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-09-29 12:20:31 +0200

    Test: check random number with range 1-1

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,8 @@
 import randomNumber from './app';
+
+describe('randomNumber', () =>{
+  it('return 1 when range 1-1', () => {
+    const result = randomNumber(1,1);
+    expect(result).toBe(1)
+  })
+})
